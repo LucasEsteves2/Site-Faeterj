6d6e4de4a1a5991843d6c14966897316f202c62e
luqui2 <lucasesteves5304@gmail.com>
2021-09-06 14:44:05 -0300

    Projeto Inicial

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).foundation();
+
+
+$('.is-hover a').on('mouseover', function () {
+  var idx = $(this).parent().index() + 2;
+  $('.info:nth-child(' + idx + ')').addClass('info-visible');
+  $('.info:nth-child(' + idx + ')').siblings().removeClass('info-visible');
+});
+
+$('.dropdown').on('mouseout', function () {
+  $('.info:nth-child(1)').addClass('info-visible').siblings().removeClass('info-visible');
+});
